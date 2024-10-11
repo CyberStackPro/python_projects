89cb37da8b74908185146b23b1a2767eaef4aa0c
CyberStackPro <yeabsragebriel@gmail.com>
2024-10-11 16:32:49 +0300

    learning inheritance

--- v1exersise/class.py
+++ v1exersise/class.py
@@ -194,5 +194,42 @@
 # # product.price = -1
 # print(product.price)
 
-class Mammal:
+
+#  Animal: Parent, Base
+#  Mammal: Child, sub
+class Animal:
+    def __init__(self):
+        print('Animal Constructor')
+        self.age = 1
+
     def eat(self):
+        print('eat')
+
+
+class Mammal(Animal):
+    def __init__(self):
+        # calling it first
+        print('Mammal Constructor')
+        self.weight = 1
+        super().__init__()
+
+        # calling it after
+        # super().__init__()
+        # print('Mammal Constructor')
+        # self.weight = 1
+
+    def walk(self):
+        print('walk')
+
+
+class Fish(Animal):
+    def swim(self):
+        print('swim')
+
+
+m = Mammal()
+# print(isinstance(m, object))
+# print(issubclass(Mammal, object))
+
+print(m.age)
+print(m.weight)
